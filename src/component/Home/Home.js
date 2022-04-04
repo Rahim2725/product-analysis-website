d2107f20509c18001c9b076318f74b23a41ff936
Rahim2725 <info.abdurrahim12@gmail.com>
2022-04-05 04:22:41 +0600

    home product added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import image from '../../logo.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='home-container'>
+                <div className='headphone-detail'>
+                    <h1>Jabra Evolve 20 SE Duo MS USB Headset</h1>
+                    <p>The Jabra Evolve 20 SE Duo MS USB Headset is a USB headset. A workplace transformation, not just headsets. The Evolve 20 Special Edition is an entry-level professional headset with superb sound for calls and music, as well as easy call management via a control device that makes, takes, and mutes call.</p>
+                    <button>Live Product</button>
+                </div>
+                <div className='headphone-img'>
+                    <img src={image} alt="logo" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Reviews = () => {
     return (
         <div>
-            
+            <h1>This Reviews </h1>
         </div>
     );
 };
