e199cc0e4d26fc70c6cedb36a49563eb214f82da
Rahim2725 <info.abdurrahim12@gmail.com>
2022-04-05 02:43:02 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Reviews from './component/Reviews/Reviews';
+import Dashboard from './component/Dashboard/Dashboard';
+import Blogs from './component/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+       <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+     </Routes>
     </div>
   );
 }
