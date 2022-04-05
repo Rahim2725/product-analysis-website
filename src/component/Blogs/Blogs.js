1622987fc8c4e79551e188b337230abacac7dbd1
Rahims609 <rahimboss609@gmail.com>
2022-04-05 17:13:01 +0600

    added

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -5,10 +5,10 @@ const Blogs = () => {
         <div>
            <div>
                <h1>What is context API</h1>
-               <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.Context provides a way to pass data through the component tree without having to pass props down manually at every level. </p>
+               <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.Context provides a way to pass data through the component tree without having to pass props down manually at every level. In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
            </div>
            <div>
-                <h1></h1>
+                <h1>What is </h1>
                 <p></p>
            </div>
            <div>
